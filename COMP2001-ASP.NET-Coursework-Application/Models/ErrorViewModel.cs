93765118e5838e53370f19d0c69aac90ec2b11ab
caleb-long19 <caleblong199@gmail.com>
2021-01-15 11:52:41 +0000

    COMP2001 Web Application - Changelog 1 - PHP & ASP.NET Projects
    Additions
    - Created ASP.NET Web Application on Visual Studio
    - Added Bootstrap support to the ASP.NET Application
    - Created PHP Application on PHP Storm
    - Added docs folder to store images

---
+++ COMP2001-ASP.NET-Coursework-Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP2001_ASP.NET_Coursework_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
